a3b0d20e1c723d2a48c09781a3d737dd707279d7
imjchiang <jchiang296@gmail.com>
2020-10-14 15:49:41 -0700

    [models] module export for BBombData in index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 
 // Mongo connection
 console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true
+//add ATLAS_URI here as option for connection when deployed
+mongoose.connect(process.env.MONGO_URI, 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
 });
 
 // Mongoose connection object
@@ -15,11 +17,13 @@ const db = mongoose.connection;
 // set up an event listener to fire once when the connections 'opens'
 // console log what host and port it's running on
 db.once('open', () => {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
 
 db.on('error', (error) => {
-  console.log(`Database error\n${error}`);
+    console.log(`Database error\n${error}`);
 });
 
+//exports of the different schemas/models
 module.exports.User = require('./User');
+module.exports.BBombData = require('./BBombData');
