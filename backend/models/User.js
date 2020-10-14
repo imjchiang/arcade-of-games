68397ace4300f2839bedcf31f8cd64eea042ed9f
imjchiang <jchiang296@gmail.com>
2020-10-14 14:42:37 -0700

    [INIT] initialize backend, initialize passport for MERN auth

---
+++ backend/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// User Schema
+const UserSchema = new Schema(
+{
+    name: 
+    {
+        type: String,
+        required: true
+    },
+    username:
+    {
+        type: String,
+        required: true
+    },
+    email: 
+    {
+        type: String,
+        required: true
+    },
+    password: 
+    {
+        type: String,
+        required: true
+    },
+    date: 
+    {
+        type: Date,
+        default: Date.now()
+    },
+    balloonBomberData:
+    {
+        type: Schema.ObjectId,
+        ref: "BBombData"
+    }
+});
+
+module.exports = User = mongoose.model('User', UserSchema);
