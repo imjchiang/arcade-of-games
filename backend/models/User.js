470b5780a0caca5f7021e65c90d97421e02a66df
imjchiang <jchiang296@gmail.com>
2020-10-14 17:56:25 -0700

    [User.js] change model reference to game data [users.js] add username verification and no duplicate user params

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,7 +29,7 @@ const UserSchema = new Schema(
         type: Date,
         default: Date.now()
     },
-    balloonBomberData:
+    bBombData:
     {
         type: Schema.ObjectId,
         ref: "BBombData"
