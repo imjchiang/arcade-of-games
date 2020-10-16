407f0be8e645b17491c4585abfa13adce35141d6
imjchiang <jchiang296@gmail.com>
2020-10-16 14:59:53 -0700

    [Profile.js] add logic for displaying games [Exploregames.js] comments on what to add [Bbomb.js] add file for game

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Profile = (props) => {
+const Profile = (props) => 
+{
+    let i = 0;
+
     console.log(props);
+
     const userData = props.user ? 
     (<div>
-        <h1>Profile</h1>
+        <h1>{props.user.username}'s Profile</h1>
+        {/* <p><strong>ID:</strong> {props.user.id}</p>  */}
         <p><strong>Name:</strong> {props.user.name}</p> 
+        <p><strong>Username:</strong> {props.user.username}</p> 
         <p><strong>Email:</strong> {props.user.email}</p> 
-        <p><strong>ID:</strong> {props.user.id}</p> 
+        
+        <>
+            {props.user.gamesPlayed
+            ? <div>
+                <h1>Game Stats</h1>
+                <>
+                    {
+                        props.user.gamesPlayed.map((eachGame, idx) =>
+                        {
+                            return(<h3>{eachGame} {idx}</h3>)
+                        })
+                    }
+                </>
+            </div>
+            :
+            <></>
+            }
+        </>
     </div>) : <h4>Loading...</h4>
 
-    const errorDiv = () => {
+    const errorDiv = () => 
+    {
         return (
             <div className="text-center pt-4">
                 <h3>Please <Link to="/login">login</Link> to view this page</h3>
