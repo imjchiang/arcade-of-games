75ef19fa90950a0220df6e90d7fbad07e88733ce
imjchiang <jchiang296@gmail.com>
2020-10-22 23:24:04 -0700

    [App.css] change hovering game piece styling [Grid.js] add game piece images when clicking and hovering, logic for switching between players

--- frontend/src/components/games/gomoku/Grid.js
+++ frontend/src/components/games/gomoku/Grid.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import GomokuBlank from "./pictures/gomoku-blank.png";
 import GomokuBlack from "./pictures/gomoku-black.png";
 import GomokuWhite from "./pictures/gomoku-white.png";
@@ -6,13 +6,65 @@ import GomokuWhite from "./pictures/gomoku-white.png";
 const Grid = (props) => 
 {
     const [grid, setGrid] = useState(Array.from({length: 19},()=> Array.from({length: 19}, () => null)));
+    const [turn, setTurn] = useState("b");
+    const [, updateState] = React.useState();
+    const forceUpdate = React.useCallback(() => updateState({}), []);
 
     const handleClick = (idx, idy) =>
     {
-        console.log(idx, idy);
+        // console.log(idx, idy);
+        let matrix = grid;
+        if (turn == "b")
+        {
+            matrix[idx][idy] = "x";
+            setTurn("w");
+        }
+        else
+        {
+            matrix[idx][idy] = "o";
+            setTurn("b");
+        }
+
+        setGrid(matrix);
+        // console.log(matrix);
+        // console.log(grid);
+        forceUpdate();
     }
 
-    const gridCreate =
+    const handleEnter = (idx, idy) =>
+    {
+        // console.log(idx, idy);
+        let matrix = grid;
+
+        if (turn == "b")
+        {
+            matrix[idx][idy] = "hoverB";
+        }
+        else
+        {
+            matrix[idx][idy] = "hoverW"
+        }
+
+        setGrid(matrix);
+        // console.log(matrix);
+        // console.log(grid);
+        forceUpdate();
+    }
+
+    const handleExit = (idx, idy) =>
+    {
+        // console.log(idx, idy);
+        let matrix = grid;
+        matrix[idx][idy] = null;
+        
+        setGrid(matrix);
+        // console.log(matrix);
+        // console.log(grid);
+        forceUpdate();
+    }       
+
+
+    return (
         <div className="gomoku-grid">
             {
                 grid.map((row, idx) =>
@@ -24,15 +76,23 @@ const Grid = (props) =>
                             {
                                 if (box === undefined || box === null)
                                 {
-                                    return <img id={idx + "," + idy} className="gomoku-box" src={GomokuBlank} onClick={() => handleClick(idx, idy)}/>
+                                    return <img className="gomoku-box" src={GomokuBlank} onClick={() => handleClick(idx, idy)} onMouseOver={() => handleEnter(idx, idy)} onMouseOut={() => handleExit(idx, idy)} />
+                                }
+                                else if (box === "hoverW")
+                                {
+                                    return <img id="hover" className="gomoku-box" src={GomokuWhite} onClick={() => handleClick(idx, idy)} onMouseOver={() => handleEnter(idx, idy)} onMouseOut={() => handleExit(idx, idy)} />
+                                }
+                                else if (box === "hoverB")
+                                {
+                                    return <img id="hover" className="gomoku-box" src={GomokuBlack} onClick={() => handleClick(idx, idy)} onMouseOver={() => handleEnter(idx, idy)} onMouseOut={() => handleExit(idx, idy)} />
                                 }
                                 else if (box === "x")
                                 {
-                                    return <img id={idx + "," + idy} className="gomoku-box" src={GomokuBlack} />
+                                    return <img className="gomoku-box" src={GomokuBlack} />
                                 }
                                 else if (box === "o")
                                 {
-                                    return <img id={idx + "," + idy} className="gomoku-box" src={GomokuWhite} />
+                                    return <img className="gomoku-box" src={GomokuWhite} />
                                 }
                                 else
                                 {
@@ -45,11 +105,6 @@ const Grid = (props) =>
                 })
             }
         </div>
-    
-    return (
-        <>
-            {gridCreate}
-        </>
     );
 }
 
