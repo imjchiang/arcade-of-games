6693751a6b3e47eb397199e14a4870b2ed29026a
imjchiang <jchiang296@gmail.com>
2020-10-23 18:50:44 -0700

    [GameConfirmQuit.js] add component, add confirm quit button and overlay, lift state

---
+++ frontend/src/components/games/gomoku/GameConfirmQuit.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const GameConfirmQuit = (props) => 
+{
+    const handleClickQ = () =>
+    {
+        let status = props.quit;
+        props.setQuit(!status);
+    }
+
+    const handleQuit = () =>
+    {
+        let statusS = props.start;
+        let statusQ = props.quit;
+        props.setStart(!statusS);
+        props.setQuit(!statusQ);
+    }
+
+    const confirmQuit = 
+        <div className="confirm-quit">
+            <h3>Are You Sure?</h3>
+            <div>
+                <button onClick={handleQuit}>Quit</button>
+                <button onClick={handleClickQ}>Continue</button>
+            </div>
+        </div>
+
+    return (
+        <>
+            {
+                props.quit
+                ?
+                confirmQuit
+                :
+                <></>
+            }
+        </>
+    );
+}
+
+export default GameConfirmQuit;
