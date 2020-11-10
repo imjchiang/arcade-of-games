44de5b68512ee0fc06aa985f42ccf7b5f5b7ba0f
imjchiang <jchiang296@gmail.com>
2020-11-09 18:00:32 -0800

    [GomEZAI.js] ai applicable to both colors, add four in row ai

--- frontend/src/components/games/gomoku/GomEZAI.js
+++ frontend/src/components/games/gomoku/GomEZAI.js
@@ -1,11 +1,32 @@
-export function threeInRowHorizNONE(grid)
+export function fourInRowHoriz(grid, piece)
 {
     for (let i = 0; i < grid.length; i++)
     {
         for (let j = 0; j < grid[i].length - 4; j++)
         {
-            if (grid[i][j] === undefined && grid[i][j + 1] === "w" && grid[i][j + 2] === "w" && 
-                grid[i][j + 3] === "w" && grid[i][j + 4] === undefined)
+            if (grid[i][j] === undefined && grid[i][j + 1] === piece && 
+                grid[i][j + 2] === piece && grid[i][j + 3] === piece && grid[i][j + 4] === piece)
+            {
+                return [i, j];
+            }
+
+            if (grid[i][j] === piece && grid[i][j + 1] === piece && 
+                grid[i][j + 2] === piece && grid[i][j + 3] === piece && grid[i][j + 4] === undefined)
+            {
+                return [i, j + 3]
+            }
+        }
+    }
+}
+
+export function threeInRowHorizNONE(grid, piece)
+{
+    for (let i = 0; i < grid.length; i++)
+    {
+        for (let j = 0; j < grid[i].length - 4; j++)
+        {
+            if (grid[i][j] === undefined && grid[i][j + 1] === piece && grid[i][j + 2] === piece && 
+                grid[i][j + 3] === piece && grid[i][j + 4] === undefined)
             {
                 if (Math.floor(Math.random()*2) === 0)
                 {
@@ -17,20 +38,20 @@ export function threeInRowHorizNONE(grid)
     }
 }
 
-export function threeInRowHorizONE(grid)
+export function threeInRowHorizONE(grid, piece)
 {
     for (let i = 0; i < grid.length; i++)
     {
         for (let j = 0; j < grid[i].length - 3; j++)
         {
-            if (grid[i][j] === undefined && grid[i][j + 1] === "w" && 
-                grid[i][j + 2] === "w" && grid[i][j + 3] === "w")
+            if (grid[i][j] === undefined && grid[i][j + 1] === piece && 
+                grid[i][j + 2] === piece && grid[i][j + 3] === piece)
             {
                 return [i, j];
             }
 
-            if (grid[i][j] === "w" && grid[i][j + 1] === "w" && 
-                grid[i][j + 2] === "w" && grid[i][j + 3] === undefined)
+            if (grid[i][j] === piece && grid[i][j + 1] === piece && 
+                grid[i][j + 2] === piece && grid[i][j + 3] === undefined)
             {
                 return [i, j + 3]
             }
