70910c72e0cc1838e0f82c6d793f1ce9b8fbf1df
imjchiang <jchiang296@gmail.com>
2020-11-09 18:35:36 -0800

    [GomEZAI.js] add vertical ai, fix positioning of four in row horiz ai

--- frontend/src/components/games/gomoku/GomEZAI.js
+++ frontend/src/components/games/gomoku/GomEZAI.js
@@ -1,3 +1,6 @@
+// ----------------------------------------------------------------------------------------------------
+//                                              HORIZONTAL                                             
+// ----------------------------------------------------------------------------------------------------
 export function fourInRowHoriz(grid, piece)
 {
     for (let i = 0; i < grid.length; i++)
@@ -13,7 +16,7 @@ export function fourInRowHoriz(grid, piece)
             if (grid[i][j] === piece && grid[i][j + 1] === piece && 
                 grid[i][j + 2] === piece && grid[i][j + 3] === piece && grid[i][j + 4] === undefined)
             {
-                return [i, j + 3]
+                return [i, j + 4]
             }
         }
     }
@@ -54,6 +57,70 @@ export function threeInRowHorizONE(grid, piece)
                 grid[i][j + 2] === piece && grid[i][j + 3] === undefined)
             {
                 return [i, j + 3]
+            }
+        }
+    }
+}
+
+// ----------------------------------------------------------------------------------------------------
+//                                               VERTICAL                                              
+// ----------------------------------------------------------------------------------------------------
+export function fourInRowVert(grid, piece)
+{
+    for (let i = 0; i < grid.length - 4; i++)
+    {
+        for (let j = 0; j < grid[i].length; j++)
+        {
+            if (grid[i][j] === undefined && grid[i + 1][j] === piece && 
+                grid[i + 2][j] === piece && grid[i + 3][j] === piece && grid[i + 4][j] === piece)
+            {
+                return [i, j];
+            }
+
+            if (grid[i][j] === piece && grid[i + 1][j] === piece && 
+                grid[i + 2][j] === piece && grid[i + 3][j] === piece && grid[i + 4][j] === undefined)
+            {
+                return [i + 4, j]
+            }
+        }
+    }
+}
+
+export function threeInRowVertNONE(grid, piece)
+{
+    for (let i = 0; i < grid.length - 4; i++)
+    {
+        for (let j = 0; j < grid[i].length; j++)
+        {
+            if (grid[i][j] === undefined && grid[i + 1][j] === piece && 
+                grid[i + 2][j] === piece && grid[i + 3][j] === piece && grid[i + 4][j] === undefined)
+            {
+                if (Math.floor(Math.random()*2) === 0)
+                {
+                    return [i, j];
+                }
+                return [i + 4, j];
+            }
+        }
+    }
+}
+
+export function threeInRowVertONE(grid, piece)
+{
+    for (let i = 0; i < grid.length - 3; i++)
+    {
+        for (let j = 0; j < grid[i].length; j++)
+        {
+            if (grid[i][j] === undefined && grid[i + 1][j] === piece && 
+                grid[i + 2][j] === piece && grid[i + 3][j] === piece)
+            {
+                return [i, j];
+            }
+
+            if (grid[i][j] === piece && grid[i + 1][j] === piece && 
+                grid[i + 2][j] === piece && grid[i + 3][j] === undefined)
+            {
+                return [i + 3, j]
             }
         }
     }
