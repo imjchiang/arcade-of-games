fe591649b8fee8c290edbeb404b5353d949f763d
imjchiang <jchiang296@gmail.com>
2020-12-03 22:13:20 -0800

    [ChkrsMove.js] add basic logic for confirming there are pieces that can be captured

--- frontend/src/components/games/checkers/ChkrsMove.js
+++ frontend/src/components/games/checkers/ChkrsMove.js
@@ -15,3 +15,55 @@ export function validMoveLight(board, selectedX, selectedY, moveToX, moveToY)
     }
     return false;
 }
+
+export function darkCanEat(board)
+{
+    let coords = [];
+    // loop through entire board
+    for (let i = 0; i < board.length; i++)
+    {
+        for (let j = 0; j < board[i].length; j++)
+        {
+            // if the piece is dark
+            if (board[i][j] === 1 || board[i][j] === 111)
+            {
+                // if the neighboring diag piece exists and is light
+                if (board[i - 1][j - 1] && board[i - 1][j - 1] === 0)
+                {
+                    coords.push({x: i - 1, y: j - 1});
+                }
+                if (board[i - 1][j + 1] && board[i - 1][j + 1] === 0)
+                {
+                    coords.push({x: i - 1, y: j - 1});
+                }
+            }
+        }
+    }
+    return coords;
+}
+
+export function lightCanEat(board)
+{
+    let coords = [];
+    // loop through entire board
+    for (let i = 0; i < board.length; i++)
+    {
+        for (let j = 0; j < board[i].length; j++)
+        {
+            // if the piece is light
+            if (board[i][j] === 0 || board[i][j] === 100)
+            {
+                // if the neighboring diag piece exists and is dark
+                if (board[i + 1][j - 1] && board[i + 1][j - 1] === 0)
+                {
+                    coords.push({x: i + 1, y: j - 1});
+                }
+                if (board[i + 1][j + 1] && board[i + 1][j + 1] === 0)
+                {
+                    coords.push({x: i + 1, y: j - 1});
+                }
+            }
+        }
+    }
+    return coords;
+}
