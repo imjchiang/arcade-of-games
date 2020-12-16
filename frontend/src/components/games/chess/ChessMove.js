6c86770d62020d0805d4bd825748b4f71f8b851f
imjchiang <jchiang296@gmail.com>
2020-12-16 01:52:53 -0800

    [ChesssMove.js] consolidate pawn movement, update console logs

--- frontend/src/components/games/chess/ChessMove.js
+++ frontend/src/components/games/chess/ChessMove.js
@@ -12,120 +12,116 @@ export function allPiecesMove(pieceX, pieceY, moveX, moveY, piece, board)
 
 export function pawnMove(pieceX, pieceY, moveX, moveY, piece, board)
 {
-    console.log("checking pawn movement");
+    console.log("---------------------------");
+    console.log("checking PAWN movement");
     if (piece.substring(0, 5) === "DPawn")
     {
-        console.log("checking dark pawn");
+        console.log("checking DARK PAWN");
         console.log("pieceX: " + pieceX + " pieceY: " + pieceY + " moveX: " + moveX + " moveY: " + moveY);
-        if (pieceX === moveX - 1 && pieceY === moveY)
+        // if piece moves in same column one space downwards
+        // if piece hops in same column two spaces downwards (confirm piece has not moved before)
+        if ((pieceX === moveX - 1 && pieceY === moveY) || 
+            (pieceX === 1 && pieceX === moveX - 2 && pieceY === moveY))
         {
-            console.log("valid norm move");
-            return true;
-        }
-        else if (pieceX === 1 && pieceX === moveX - 2 && pieceY === moveY)
-        {
-            console.log("valid hop move");
+            console.log("valid DARK PAWN movement");
             return true;
         }
     }
     else if (piece.substring(0, 5) === "LPawn")
     {
-        console.log("checking light pawn");
+        console.log("checking LIGHT PAWN");
         console.log("pieceX: " + pieceX + " pieceY: " + pieceY + " moveX: " + moveX + " moveY: " + moveY);
-        if (pieceX === moveX + 1 && pieceY === moveY)
+        if ((pieceX === moveX + 1 && pieceY === moveY) || 
+            (pieceX === board.length - 2 && pieceX === moveX + 2 && pieceY === moveY))
         {
-            console.log("valid norm move");
-            return true;
-        }
-        else if (pieceX === board.length - 2 && pieceX === moveX + 2 && pieceY === moveY)
-        {
-            console.log("valid hop move");
+            console.log("valid LIGHT PAWN movement");
             return true;
         }
     }
-    console.log("invalid move");
+    console.log("invalid PAWN move");
     return false;
 }
 
 export function rookMove(pieceX, pieceY, moveX, moveY, piece, board)
 {
-    console.log("checking rook movement");
+    console.log("---------------------------");
+    console.log("checking ROOK movement");
     // need to implement for castling later
     // if (piece.substring(0, 5) === "DRook")
     // else if (piece.substring(0, 5) === "LRook")
-    if (piece.substring(1, 5) === "Rook")
+    console.log("pieceX: " + pieceX + " pieceY: " + pieceY + " moveX: " + moveX + " moveY: " + moveY);
+    if (pieceX === moveX && moveY > pieceY)
     {
-        // console.log("checking dark rook");
-        console.log("pieceX: " + pieceX + " pieceY: " + pieceY + " moveX: " + moveX + " moveY: " + moveY);
-        if (pieceX === moveX && moveY > pieceY)
+        console.log("ROOK moving RIGHT");
+        for (let i = pieceY + 1; i <= moveY; i++)
         {
-            for (let i = pieceY + 1; i <= moveY; i++)
+            if (board[moveX][i] === null)
             {
-                if (board[moveX][i] === null)
-                {
-                    console.log("no piece found");
-                }
-                else
-                {
-                    console.log("piece obstructing");
-                    return false;
-                }
+                console.log("no piece obstructing rook");
             }
-            console.log("valid norm move");
-            return true;
-        }
-        else if (pieceX === moveX && moveY < pieceY)
-        {
-            for (let i = moveY; i < pieceY; i++)
+            else
             {
-                if (board[moveX][i] === null)
-                {
-                    console.log("no piece found");
-                }
-                else
-                {
-                    console.log("piece obstructing");
-                    return false;
-                }
+                console.log("PIECE OBSTRUCTING ROOK");
+                return false;
             }
-            console.log("valid norm move");
-            return true;
         }
-        else if (pieceY === moveY && moveX > pieceX)
-        {
-            for (let i = pieceX + 1; i <= moveX; i++)
-            {
-                if (board[i][moveY] === null)
-                {
-                    console.log("no piece found");
-                }
-                else
-                {
-                    console.log("piece obstructing");
-                    return false;
-                }
-            }
-            console.log("valid norm move");
-            return true;
-        }
-        else if (pieceY === moveY && moveX < pieceX)
-        {
-            for (let i = moveX; i < pieceX; i++)
-            {
-                if (board[i][moveY] === null)
-                {
-                    console.log("no piece found");
-                }
-                else
-                {
-                    console.log("piece obstructing");
-                    return false;
-                }
-            }
-            console.log("valid norm move");
-            return true;
-        }
+        console.log("valid ROOK movement");
+        return true;
     }
-    console.log("invalid move");
+    else if (pieceX === moveX && moveY < pieceY)
+    {
+        console.log("ROOK moving LEFT");
+        for (let i = moveY; i < pieceY; i++)
+        {
+            if (board[moveX][i] === null)
+            {
+                console.log("no piece obstructing rook");
+            }
+            else
+            {
+                console.log("PIECE OBSTRUCTING ROOK");
+                return false;
+            }
+        }
+        console.log("valid ROOK movement");
+        return true;
+    }
+    else if (pieceY === moveY && moveX > pieceX)
+    {
+        console.log("ROOK moving DOWN");
+        for (let i = pieceX + 1; i <= moveX; i++)
+        {
+            if (board[i][moveY] === null)
+            {
+                console.log("no piece obstructing rook");
+            }
+            else
+            {
+                console.log("PIECE OBSTRUCTING ROOK");
+                return false;
+            }
+        }
+        console.log("valid ROOK movement");
+        return true;
+    }
+    else if (pieceY === moveY && moveX < pieceX)
+    {
+        console.log("ROOK moving UP");
+        for (let i = moveX; i < pieceX; i++)
+        {
+            if (board[i][moveY] === null)
+            {
+                console.log("no piece obstructing rook");
+            }
+            else
+            {
+                console.log("PIECE OBSTRUCTING ROOK");
+                return false;
+            }
+        }
+        console.log("valid ROOK movement");
+        return true;
+    }
+    console.log("invalid ROOK move");
     return false;
 }
