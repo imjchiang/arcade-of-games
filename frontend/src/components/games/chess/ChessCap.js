02c245a28d7f8c3347e11d7fefc173f8e078600d
imjchiang <jchiang296@gmail.com>
2020-12-19 04:04:45 -0800

    [ChessCap.js] implement rook capture

--- frontend/src/components/games/chess/ChessCap.js
+++ frontend/src/components/games/chess/ChessCap.js
@@ -6,7 +6,7 @@ export function allPiecesCap(pieceX, pieceY, moveX, moveY, piece, board)
     }
     if (piece.substring(1, 5) === "Rook")
     {
-        return;
+        return rookCap(pieceX, pieceY, moveX, moveY, piece, board);
     }
     if (piece.substring(1, 7) === "Knight")
     {
@@ -54,4 +54,89 @@ export function pawnCap(pieceX, pieceY, moveX, moveY, piece, board)
     }
     console.log("invalid PAWN capture");
     return false;
+}
+
+export function rookCap(pieceX, pieceY, moveX, moveY, piece, board)
+{
+    console.log("---------------------------");
+    console.log("checking ROOK capture");
+    // need to implement for castling later
+    console.log("pieceX: " + pieceX + " pieceY: " + pieceY + " moveX: " + moveX + " moveY: " + moveY);
+    if ((piece.substring(0, 5) === "DRook" && board[moveX][moveY].substring(0, 1) !== "D") || (piece.substring(0, 5) === "LRook" && board[moveX][moveY].substring(0, 1) !== "L"))
+    {
+        if (pieceX === moveX && moveY > pieceY)
+        {
+            console.log("ROOK capturing RIGHT");
+            for (let i = pieceY + 1; i < moveY; i++)
+            {
+                if (board[moveX][i] === null)
+                {
+                    console.log("no piece obstructing rook");
+                }
+                else
+                {
+                    console.log("PIECE OBSTRUCTING ROOK");
+                    return false;
+                }
+            }
+            console.log("valid ROOK capture");
+            return true;
+        }
+        else if (pieceX === moveX && moveY < pieceY)
+        {
+            console.log("ROOK capturing LEFT");
+            for (let i = moveY + 1; i < pieceY; i++)
+            {
+                if (board[moveX][i] === null)
+                {
+                    console.log("no piece obstructing rook");
+                }
+                else
+                {
+                    console.log("PIECE OBSTRUCTING ROOK");
+                    return false;
+                }
+            }
+            console.log("valid ROOK capture");
+            return true;
+        }
+        else if (pieceY === moveY && moveX > pieceX)
+        {
+            console.log("ROOK capturing DOWN");
+            for (let i = pieceX + 1; i < moveX; i++)
+            {
+                if (board[i][moveY] === null)
+                {
+                    console.log("no piece obstructing rook");
+                }
+                else
+                {
+                    console.log("PIECE OBSTRUCTING ROOK");
+                    return false;
+                }
+            }
+            console.log("valid ROOK capture");
+            return true;
+        }
+        else if (pieceY === moveY && moveX < pieceX)
+        {
+            console.log("ROOK capturing UP");
+            for (let i = moveX + 1; i < pieceX; i++)
+            {
+                if (board[i][moveY] === null)
+                {
+                    console.log("no piece obstructing rook");
+                }
+                else
+                {
+                    console.log("PIECE OBSTRUCTING ROOK");
+                    return false;
+                }
+            }
+            console.log("valid ROOK capture");
+            return true;
+        }
+        console.log("invalid ROOK move");
+        return false;
+    }
 }
