85c95b67aea3bf8a81dab03ae74df1653977e1d2
imjchiang <jchiang296@gmail.com>
2021-01-21 14:27:49 -0800

    [About.js] add info about the website

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,13 +3,23 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
-            <p>This is all about us.</p>
+            <h1>About rKD</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Tempore dolorum excepturi, pariatur dolore fuga perferendis 
-                mollitia deserunt a voluptas assumenda! Consequatur beatae qui 
-                dolorem tempora possimus accusantium, fugit eius quidem?
+                rKD is an "online arcade" where users like you can come and play board games listed under our "Play Games" tab. 
+                We offer a selection of board games ranging from Chess to Gomoku and encourage board game enthusiasts to come 
+                and join our community to play with friends and family, or even with other people online.
+            </p>
+
+            <h1>Development</h1>
+            <p>
+                rKD was developed using the MERN technology stack (MongoDB, Express.js, React.js, Node.js). 
+            </p>
+
+            <h1>About Me</h1>
+            <p>
+                I'm Josh and I am the developer of rKD. I enjoy playing board games and video games and decided it would be really 
+                cool to have a site where users would be able to find all sorts of board games in one place and be able to play all
+                these games in one place. Some things I enjoy besides games are music, teaching, and advances in biochemical research.
             </p>
         </div>
     );
